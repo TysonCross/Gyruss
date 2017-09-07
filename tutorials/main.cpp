a4fed1dd9cd4bca658c346cbccdeb2fa2c262434
Tyson <tyson.cross@gmail.com>
2017-09-07 17:37:10 +0200

    SFML Tutorial/explore

---
+++ tutorials/main.cpp
@@ -0,0 +1,18 @@
+//--------------------------------------------------------------------------
+// Student  1239448
+// @file    main.cpp
+// @author  Tyson Cross
+// @date    6/9/17
+// @brief   Exploratory SFML code
+//
+// Copyright (c) 2017 Tyson Cross, Wits University, All rights reserved.
+//--------------------------------------------------------------------------
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
