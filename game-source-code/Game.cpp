3b0cf8c122c508ce2bb570b2415172d930832fe5
Tyson <tyson.cross@gmail.com>
2017-09-09 11:51:49 +0200

    Ship moves around circle, oriented to angle

--- game-source-code/Game.cpp
+++ game-source-code/Game.cpp
@@ -13,15 +13,17 @@
 #include <iostream>
 #include "Game.hpp"
 #include "SplashScreen.hpp"
+#include <cmath>
 
 Game::GameState Game::_gameState = Splash;
 sf::RenderWindow Game::_mainWindow;
-const int gameWidth = 1080;
+
+const int gameWidth = 1920;
 const int gameHeight = 1080;
+const float pi = 3.1415;
 
-void Game::Start(void)
+void Game::Start()
 {
-
     sf::Image icon;
     icon.loadFromFile("resources/icon.png");
 
@@ -29,8 +31,9 @@ void Game::Start(void)
                        sf::Style::Titlebar | sf::Style::Close);
     _mainWindow.setKeyRepeatEnabled(true);
     _mainWindow.setMouseCursorVisible(false);
+    //_mainWindow.setMouseCursorGrabbed(true);
     _mainWindow.setVerticalSyncEnabled(true);
-    _mainWindow.setMouseCursorGrabbed(true);
+    _mainWindow.setFramerateLimit(60);
     _mainWindow.setIcon(32, 32, icon.getPixelsPtr());
 
     _gameState = Splash;
@@ -44,6 +47,8 @@ void Game::Start(void)
 
 void Game::GameLoop()
 {
+    int shipAngle = 0;
+    const auto shipPathRadius = (gameHeight/2)-(gameHeight*0.1);
     sf::Event event;
     sf::Clock clock;
     sf::Color color(sf::Color::Black);
@@ -55,8 +60,8 @@ void Game::GameLoop()
     }
 
     sf::Sprite ship(texture);
+    ship.setScale(0.5,0.5);
     ship.setOrigin(ship.getGlobalBounds().width/2,ship.getGlobalBounds().height/2);
-    ship.setPosition(gameWidth/2, gameHeight/2);
 
     if (_gameState == Game::Splash)
     {
@@ -82,24 +87,48 @@ void Game::GameLoop()
                             break;
                         }
                     if ((event.key.code == sf::Keyboard::A) || (event.key.code == sf::Keyboard::Left))
-                        ship.move(-5, 0);
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)
+                            || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
+                            shipAngle -= 5;
+                        else
+                            shipAngle -= 2;
 
                     if ((event.key.code == sf::Keyboard::D) || (event.key.code == sf::Keyboard::Right))
-                        ship.move(5, 0);
+                        if (sf::Keyboard::isKeyPressed(sf::Keyboard::LShift)
+                            || sf::Keyboard::isKeyPressed(sf::Keyboard::RShift))
+                            shipAngle += 5;
+                        else
+                            shipAngle += 2;
+
+                    if (event.key.code == sf::Keyboard::Space)
+                    {
+                        shipAngle = 0;
+                    }
 
                     break;
                 }
             }
+        } // End of input polling
 
-            std::cout << "Elapsed time since last frame: " << clock.getElapsedTime().asMicroseconds() << std::endl;
+            std::cout << "Angle is: " << shipAngle << " and position: (" << ship.getPosition().x << ", " << ship.getPosition().y << ")";
+            std::cout << " Elapsed time since last frame: " << clock.getElapsedTime().asMicroseconds() << std::endl;
             clock.restart();
+
+            shipAngle = shipAngle % 360;
+            if(shipAngle<0)shipAngle+=360;
+            //Rotate coordinate system by 90 degrees
+            ship.setPosition(shipPathRadius*sin(shipAngle*(pi/180))+gameWidth/2,shipPathRadius*cos(shipAngle*(pi/180))+gameHeight/2);
+            ship.setRotation(-shipAngle);
+
+            //Send all inputs to the inputController
+
+            //Get all objects to be drawn
 
             _mainWindow.clear(color);
             _mainWindow.draw(ship);
             _mainWindow.display();
         }
     }
-}
 
 void Game::ShowSplashScreen()
 {
