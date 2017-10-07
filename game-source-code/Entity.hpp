c1c37216b2755b44e366397307f7f80b3e9b2fbb
Tyson <tyson.cross@gmail.com>
2017-10-07 14:15:27 +0200

    New base classes Moveable and Animatable
    Entity class now inherits from these two "component" classes.

--- game-source-code/Entity.hpp
+++ game-source-code/Entity.hpp
@@ -4,7 +4,7 @@
 /// \date    14/9/17
 /// \brief   Basic entity class for game objects
 ///
-/// Enemies, lasers, missiles, and asteroids will all inherit from this class
+/// Enemies, lasers, missiles, and asteroids all inherit from this class
 ///
 /// \copyright (c) 2017 Tyson Cross and Chris Maree, Wits University
 /////////////////////////////////////////////////////////////////////
@@ -15,53 +15,36 @@
 ////////////////////////////////////////////////////////////
 // Headers
 ////////////////////////////////////////////////////////////
-#include "SFML/Graphics.hpp"
 #include <cmath>
 #include "common.hpp"
-#include "ResourceHolder.hpp"
+#include "Moveable.hpp"
+#include "Animatable.hpp"
 
-class Entity
+class Entity : public Moveable, public Animatable
 {
 public:
-    Entity();
     Entity(const sf::Vector2i resolution,
            float distanceFromCentre,
            float angle,
            float scale,
            const entity::ID type,
-           const TextureHolder &textureHolder) : _resolution{resolution},
-                                                 _distanceFromCentre{distanceFromCentre},
-                                                 _angle{angle},
-                                                 _scale{scale},
-                                                 _type{type}
-    {}
+           const TextureHolder &textureHolder) : Moveable{distanceFromCentre,
+                                                          angle,
+                                                          scale},
+                                                 Animatable{textureHolder,
+                                                            type},
+                                                 _resolution{resolution} {}
 
-    virtual void setMove(float){}
-    virtual void setMove(float, float){}
+    virtual const int getLives() const=0;
     virtual void reset()=0;
     virtual void update()=0;
-    virtual const float getRadius() const=0;
-    virtual const float getDistanceFromCentre()const=0;
-    virtual const sf::Vector2f getScale()const=0;
-    virtual const sf::Vector2f getPosition()const=0;
-    virtual const sf::Sprite &getSprite()const=0;
-    virtual const int getLives() const=0;
     virtual void die()=0;
     virtual void move()=0;
 
 protected:
     const sf::Vector2i _resolution;
-    sf::Sprite _sprite;
-    float _distanceFromCentre;
-    float _futureAngleValue;
-    float _futureMoveValue;
-    float _angle;
-    float _scale;
-    bool _isMoving;
     int _lives;
-    textures::ID _id;
-    entity::ID _type;
-    int _animationFPSLimit;
+
 };
 
 
